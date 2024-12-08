10668d5129c00038dab875b8d0e1241b9df5b4b4
Hannifa Indah Rahayu <148038219+locatny@users.noreply.github.com>
2024-12-08 20:09:25 +0700

    Update Credit.java

--- Credit.java
+++ Credit.java
@@ -1,5 +1,18 @@
 public class Credit extends Payment {
     private String number;
     private String type;
-    private Date expDate
+    private String expDate;
+
+    public Credit(double amount, String number, String type, String expDate) {
+        super(amount);
+        this.number = number;
+        this.type = type;
+        this.expDate = expDate;
+    }
+
+    @Override
+    public boolean authorized() {
+        // For simplicity, assume all credit payments are authorized
+        return true;
+    }
 }
